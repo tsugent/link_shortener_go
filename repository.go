ed833d47f76acfb4406cfdfe255c3720c5b0a2e4
tony <tsugent@gmail.com>
2024-08-11 18:44:01 -0400

    add create and get functions

--- repository.go
+++ repository.go
@@ -1,6 +1,12 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 // definte methods URLRepositoryInterface must have
 type URLRepositoryInterface interface {
@@ -14,9 +20,25 @@ type URLRepository struct {
 }
 
 func (repo *URLRepository) Create(url string) (URL, error) {
-	return
+	var createdURL URL
+	uuidString := uuid.NewString()
+	redirectToPath, _, _ := strings.Cut(uuidString, "-")
+
+	sqlQuery := `INSERT INTO urls (original_url, uuid_string, created) VALUES (?, ?, ?)`
+	_, err := repo.db.Exec(sqlQuery, url, redirectToPath, time.Now().String())
+	if err != nil {
+		return createdURL, err
+	}
+	return repo.Get(redirectToPath)
 }
 
 func (repo *URLRepository) Get(urlUUID string) (URL, error) {
-	return
+	var url URL
+	sqlQuery := `SELECT original_url, uuid_string, created FROM urls WHERE uuid_string = ?`
+	row := repo.db.QueryRow(sqlQuery, urlUUID)
+	err := row.Scan(&url.OriginalURL, &url.UUIDString, &url.Created)
+	if err != nil {
+		return url, err
+	}
+	return url, nil
 }
